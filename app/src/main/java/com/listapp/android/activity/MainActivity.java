77af0909713db11d98d871089823a630f2954dbf
Ivan Pico <ipico@mediktor.com>
2017-04-11 11:18:48 +0100

    First modifications .

--- app/src/main/java/com/listapp/android/activity/MainActivity.java
+++ app/src/main/java/com/listapp/android/activity/MainActivity.java
@@ -3,14 +3,11 @@ package com.listapp.android.activity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.listapp.android.R;
 import com.listapp.android.activity.adapter.ItemAdapter;
 import com.listapp.android.activity.adapter.RecycleViewAdapter;
+import com.listapp.android.global.ApiServer;
 import com.listapp.android.model.openweathermap.WeatherGlobalData;
 import com.listapp.android.model.openweathermap.WeatherList;
 import com.listapp.android.presenter.DataItemInteractorImpl;
@@ -40,20 +37,20 @@ public class MainActivity extends BasicActivity implements WeatherListView{
         adapter = new ItemAdapter(this, weatherLists);
 
         // Attach the adapter to a ListView
-        ListView listView = (ListView) findViewById(R.id.lt_main_activity);
-        listView.setAdapter(adapter);
+//        ListView listView = (ListView) findViewById(R.id.lt_main_activity);
+//        listView.setAdapter(adapter);
 
         DataItemInteractorImpl interactor = new DataItemInteractorImpl();
         presenter = new DataItemPresenter(interactor);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                Log.d(TAG,"Item clicked");
-
-            }
-        });
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//                Log.d(TAG,"Item clicked");
+//
+//            }
+//        });
 
 
         mRecyclerView = (RecyclerView) findViewById(R.id.rv_main_activity);
@@ -78,7 +75,7 @@ public class MainActivity extends BasicActivity implements WeatherListView{
 
         presenter.bind(this);
 
-        presenter.prefomDataWeather("3128759");
+        presenter.prefomDataWeather(ApiServer.CITY_ID_BARCELONA);
 
     }
     @Override
--- app/src/main/java/com/listapp/android/global/ApiServer.java
+++ app/src/main/java/com/listapp/android/global/ApiServer.java
@@ -8,7 +8,8 @@ public class ApiServer {
     * http://api.openweathermap.org/data/2.5/forecast?id=3128759&APPID=98d05cb13bc73c66c5099fa4481d7a41
     */
 
-    public final static String API_KEY = "98d05cb13bc73c66c5099fa4481d7a41";
+    public final static String API_KEY = "7855ff73bf07f8dc5cb11c3c93e28c24";
     public final static String API_CONTENT_TYPE = "application/json";
 
+    public static final String CITY_ID_BARCELONA = "3128759";
 }
